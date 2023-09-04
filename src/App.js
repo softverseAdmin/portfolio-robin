f3cbfc5068971a39f1e99cde530af13a78ec531a
sudip khatri <sudpkhatri4@gmail.com>
2023-09-04 12:47:46 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './assets/css/App.css';
 import './assets/js/App.js';
-import Navbar from './components/header/Navbar';
+import Navigation from './components/header/Navigation';
+import Navbody from './components/header/Navbody';
+import Contact from './components/Contact/Contact';
+import Projects from './components/Projects/Projects';
+import About from './components/About/About';
+import FullScreenMenu from './components/header/FullscreenMenu.jsx';
 
 function App() {
+  // get the web url: http://localhost:3000/about
+  const url = window.location.href;
+  // get an array using split and use last index as the address
+  const add = url.split("/");
+  const webAdd = add[add.length - 1];
+
   return (
     <div className="App">
-      <Navbar />
+      <div className="header head">
+        <FullScreenMenu />
+      </div>
+      <div className="body">
+        {/* conditional statement, comapare & return component */}
+        {webAdd === "home" && <Navbody/>}
+        {webAdd === "" && <Navbody />}
+        {webAdd === "projects" && <Projects />}
+        {webAdd === "contact" && <Contact />}
+        {webAdd === "about" && <About />}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
