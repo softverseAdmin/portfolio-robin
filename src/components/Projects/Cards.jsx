227894b43612f355d6e49dc9d21992c6fdd71d91
sudip khatri <sudpkhatri4@gmail.com>
2023-09-10 00:05:31 +1000

    feature: cards, projects

--- src/components/Projects/Cards.jsx
+++ src/components/Projects/Cards.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import './Cards.css';
 
-const CardAnimatedBorderGradient = () => {
+const CardAnimatedBorderGradient = ({customer, url, img}) => {
   return (
     <div className="card">
       <span className="borderGradient" />
-      <div className="textContent">Lorem ipsum dolor sit amet consectetur 
-      adipisicing elit. Enim at facilis mollitia, 
-      voluptates illum illo sequi ad cupiditate! Distinctio ducimus 
-      sit perspiciatis voluptatibus deserunt dicta voluptatem 
-      officiis nobis eum alias.</div>
+      <div
+        className="textContent border"
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition:'center',
+          backgroundSize: 'cover',
+        }}
+      ></div>
     </div>
   );
 };
