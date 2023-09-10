08f47d08a371deb278179fd5253a8b7392cea492
sudip khatri <sudpkhatri4@gmail.com>
2023-09-10 16:37:49 +1000

    feature: react swiper

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Cards from "./Cards";
 import img from "../../assets/videos/ntg.png";
-import { Navigation, Pagination, Scrollbar, A11y} from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Keyboard,
+} from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -39,15 +45,35 @@ export default function Projects() {
     <div className="container-fluid d-flex align-items-center vh-100">
       <div className="container flex-column">
         <Swiper
-        
-          modules={[Navigation, Pagination, Scrollbar, A11y,]}
-          spaceBetween={200}
           slidesPerView={3}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
+          centeredSlides={false}
+          slidesPerGroupSkip={1}
+          grabCursor={true}
+          keyboard={{
+            enabled: true,
+          }}
+          breakpoints={{
+            769: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+            },
+          }}
+          scrollbar={true}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+          className="mySwiper"
+
+          // modules={[Navigation, Pagination, Scrollbar, A11y,]}
+          // spaceBetween={200}
+          // slidesPerView={3}
+          // navigation
+          // pagination={{ clickable: true }}
+          // scrollbar={{ draggable: true }}
+          // onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={() => console.log("slide change")}
         >
           {data.map((d, index) => {
             const { customer, url, img } = d;
