77cb89cf38473c09cb3841548216f59ef80b3b84
softverseAdmin <softverse4@gmail.com>
2023-09-10 20:13:54 +0900

    conflict

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,47 +1,65 @@
 import React, { useState, useMemo } from 'react'
 import Select from 'react-select'
 import countryList from 'react-select-country-list'
+import { Container, Row, Form, Button } from 'react-bootstrap';
 
 export default function Contact() {
-  const [value, setValue] = useState('')
+  const [valueCountry, setCountryValue] = useState('')
+  const [valueLang, setLangValue] = useState('')
   const options = useMemo(() => countryList().getData(), [])
 
-  const changeHandler = value => {
-    setValue(value)
+  const changeCountryHandler = valueCountry => {
+    setCountryValue(valueCountry)
   }
+  const changeLangHandler = valueLang => {
+    setLangValue(valueLang)
+  }
+  const LanguageOption = [
+    { value: '0', label: 'EN-US' },
+    { value: '1', label: 'JP-JPN' },
+    { value: '2', label: 'KR-KRN' }
+  ]
   return (<>
-    <form className='float-start align-left text-start'>
-      <div className="mb-3">
-        <label htmlFor="txtInputName" className="form-label">Full Name</label>
-        <input type="text" className="form-control" id="txtInputName" aria-describedby="Name" required/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="txtCompanyName" className="form-label">Company Name</label>
-        <input type="text" className="form-control" id="txtCompanyName" aria-describedby="Name" required/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="selectCountry" className="form-label">Country</label>
-        <Select options={options} value={value} id="selectCountry" onChange={changeHandler} />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-      </div>
-      <div class="mb-3">
-        <label for="charMessage" class="form-label">Message</label>
-        <textarea class="form-control" id="charMessage" rows="3"></textarea>
-      </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div>
-      <div className="mb-3">
-          <label htmlFor="recaptcha" className="form-label">
-            Security checks
-          </label>
-        </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+    <Container fluid className="d-flex align-items-center justify-content-center" style={{ height: '100vh', margin: 'auto' }}>
+      <Row className='w-75 h-50 overflow-scroll z-index-2'>
+          <Form>
+            <Form.Group controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your first name" />
+            </Form.Group>
+            <Form.Group controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your last name" />
+            </Form.Group>
+            <Form.Group controlId="companyName">
+              <Form.Label>Company Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your company name" />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" />
+            </Form.Group>
+            <Form.Group controlId="contactNumber">
+              <Form.Label>Contact Number</Form.Label>
+              <Form.Control type="tel" placeholder="Enter your contact number" />
+            </Form.Group>
+            <Form.Group controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+            </Form.Group>
+            <Form.Group controlId="countrySelect">
+              <Form.Label>Country Select</Form.Label>
+              <Select className='text-dark ' options={options} value={valueCountry} id="countrySelect" onChange={changeCountryHandler} />
+            </Form.Group>
+            <Form.Group controlId="languageSelect">
+              <Form.Label>Language Select</Form.Label>
+              <Select className='text-dark ' options={LanguageOption} value={valueLang} id="languageSelect" onChange={changeLangHandler} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+      </Row>
+    </Container>
   </>)
 }
