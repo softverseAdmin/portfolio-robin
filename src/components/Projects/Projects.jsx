4aa8be636ed2b0e1e381f66811924953a669b3a7
sudip khatri <sudpkhatri4@gmail.com>
2023-09-10 16:16:16 +1000

    feature: update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import Cards from './Cards';
+import React from "react";
+import Cards from "./Cards";
 import img from "../../assets/videos/ntg.png";
+import { Navigation, Pagination, Scrollbar, A11y} from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 
 export default function Projects() {
-
   const data = [
     {
       customer: "Nepal Travel Guide",
@@ -20,25 +29,36 @@ export default function Projects() {
       url: "https://www.nepal-travel-guide.com/",
       img: img,
     },
-    
+    {
+      customer: "Travel Guide",
+      url: "https://www.nepal-travel-guide.com/",
+      img: img,
+    },
   ];
   return (
     <div className="container-fluid d-flex align-items-center vh-100">
-      <div className="container flex-column gap-5">
-        <div className="row gap-4">
+      <div className="container flex-column">
+        <Swiper
+        
+          modules={[Navigation, Pagination, Scrollbar, A11y,]}
+          spaceBetween={200}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
           {data.map((d, index) => {
             const { customer, url, img } = d;
             return (
-              <div className="col" key={index}>
-                <Cards
-                  customer={customer}
-                  url={url}
-                  img={img}
-                />
-              </div>
+              <SwiperSlide key={index}>
+                <Cards customer={customer} url={url} img={img} />
+              </SwiperSlide>
             );
           })}
-        </div>
+        </Swiper>
       </div>
     </div>
-  );}
+  );
+}
